b8fe0c33bd6188213e4e31211638c927f3f7cfb2
unknown <tashfeenc1234@gmail.com>
2019-12-26 13:01:29 +0600

    Second commit

--- app.js
+++ app.js
@@ -11,8 +11,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use('/', indexRouter);
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 module.exports = app;
